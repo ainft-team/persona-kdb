7b3050a22ed34ae7b7fa8f4032d1b0f11c93caf4
Jungwoo Pyo <jungwoo_pyo@comcom.ai>
2023-11-24 13:33:52 +0900

    feat: fix schema and enums for firebase

---
+++ persona_kdb/api/enums.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+# Status
+class ConversationStatus(str, Enum):
+    PREPARED = "prepared"
+    IN_PROGRESS = "in_progress"
+class MessageStatus(str, Enum):
+    SUCCESS = "success"
+    ERROR = "error"
+    IN_PROGRESS = "in_progress"
+class SoulstoneRewardStatus(str, Enum):
+    ACCEPTED = "accepted"
+    PAID = "paid"
+    REJECTED = "rejected"
+
+# Type
+class UserLevel(str, Enum):
+    ADMIN = "admin"
+    HOLDER = "holder"
+    GUEST = "guest"
+class RoleType(str, Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
+class MessageType(str, Enum):
+    USER_FIRST = "user_first"
+    ASSISTANT_FIRST = "assistant_first"
+    USER_REPLY = "user_reply"
+    ASSISTANT_REPLY = "assiatant_reply"
+class ConversationType(str, Enum):
+    START_BY_USER = "start_by_user"
+    USER_MENTION_BY_ASSISTANT = "user_mention_by_assistant"
+    PUBLIC_BY_ASSISTANT = "public_by_assistant"
---
+++ persona_kdb/api/schema.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+from typing import Optional, Union
+from datetime import datetime
+
+from api.enums import MessageType, ConversationType
+#####
+# Schema for firebase
+#####
+#"v2_rewards"
+# the key of the document is the tx_id
+class SoulStoneRewardTx(BaseModel):
+    tx_id: str
+    amount: int
+    recipient_id: str
+    status: str
+    created_at: Union[float, str, datetime]
+#"v2_users"
+# the key of the document is the user's discord id
+class User(BaseModel):
+    id: str
+    name: str
+    discriminator: Optional[str]
+    level: str
+    role: str
+    message_count_with_mars: int
+    conversation_count_with_mars: int
+    soulstone_reward: int
+    soulstone_reward_txs: Optional[dict[str, SoulStoneRewardTx]] # {tx_id: SoulStoneRewardTx}
+#"v2_messages"
+# the key of the document is the message id
+class Message(BaseModel):
+    type: MessageType
+    content: str
+    status: str
+    created_at: Union[float, str, datetime]
+    sender_id: str
+    parent_message_id: Optional[str] # If none, it's the root message
+#"v2_conversations"
+# the key of the document is the first message id
+class Conversation(BaseModel):
+    type: ConversationType    
+    status: Optional[str]
+    message_count: Optional[int]
+    created_at: Optional[Union[float, str, datetime]]
+    updated_at: Optional[Union[float, str, datetime]]
+    starter_user: str # Discord ID of the user or bot who started the conversation
+    participants: dict[str, int] # {discord_id: message_count}
+    messages: dict[str, Message] # {message_id: Message}
